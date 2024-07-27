7ab56fdcfc113a4cfc74bdd0bbc5c04c8cfc10f3
gopal-vsg <saigopalvallu7@gmail.com>
2024-07-27 18:57:22 +0530

    the properties file is created and entity,repository file is created

---
+++ src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,57 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private double price;
+    private int quatity;
+    private String origin;
+
+    public Product() {
+    }
+
+    public Product(String name, double price, int quatity, String origin) {
+        this.name = name;
+        this.price = price;
+        this.quatity = quatity;
+        this.origin = origin;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    public int getQuatity() {
+        return quatity;
+    }
+    public void getQuatity(int quatity) {
+        this.quatity = quatity;
+    }
+    public String getOrigin() {
+        return origin;
+    }
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+}
